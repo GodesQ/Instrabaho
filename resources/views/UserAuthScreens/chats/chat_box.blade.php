ac8f897b7d7895fb90a75803179a0ab6fdef5e25
gabsablad <gabsablad@gmail.com>
2023-02-22 11:49:31 +0800

    Added Chat Page

---
+++ resources/views/UserAuthScreens/chats/chat_box.blade.php
@@ -0,0 +1,37 @@
+@extends('layout.user-layout')
+
+@section('content')
+    <div class="page-wrapper">
+            <div class="page-content">
+                <div class="page-body">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-6">
+                                <div class="card">
+                                    <div class="card-body">
+
+                                        <div class="chat-header">
+                                            <div class="active-chat-header"> Projects</div>
+                                                <div> Services </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="col-xl-6">
+                                <div class="card">
+                                    <div class="card-body">
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+@endsection
+
