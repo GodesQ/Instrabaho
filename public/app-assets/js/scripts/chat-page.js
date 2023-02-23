b8fa892450f06159e9390255f4d00aa3ac6baf81
gabsablad <gabsablad@gmail.com>
2023-02-23 10:59:48 +0800

    Updated UI and added chatbox

---
+++ public/app-assets/js/scripts/chat-page.js
@@ -0,0 +1,12 @@
+let chat_header_tabs = document.querySelectorAll('.chat-header-tab');
+
+chat_header_tabs.forEach(tab => {
+    tab.addEventListener('click', function(e) {
+        chat_header_tabs.forEach(single_tab => {
+            single_tab.classList.remove('active-chat-header');
+        })
+
+        this.classList.add('active-chat-header');
+    })
+})
+
